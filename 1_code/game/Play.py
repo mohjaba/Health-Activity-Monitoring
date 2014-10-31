5af41fedabaac1868a2f5fec2f502cb5763f45ca
iparask <ioannis.paraskevakos@rutgers.edu>
2014-10-31 01:15:10 -0400

    Commented files

--- 1_code/game/Play.py
+++ 1_code/game/Play.py
@@ -14,23 +14,81 @@ class Play:
 	
 	"""
 	This Class is the Class that conects MongoDB with the
-	Game class.
-	"""
+	Game class. Contains all the necessary the methods to enable the game.
+	
+	** *Attributes* **
+	   mongoclient   : This is the actual connection with the MongoDB
+	   CollState     : This is the connection with the Collection that
+                           holds the data for the Game over the States.
+           CollTwets     : This is the connection with the Collection that 
+                           holds the data from the tweets pulled from Twitter
+           TweetedStates : This attribute has a list of the states mentioned
+                                in the Tweets
 
-	mongoclient = MongoClient('mongodb://localhost:27017') # Creates aa connection to MongoDB
-	db = client.search_results #Selects a database
+        ** *Methods* **
+           __init__ : The constructor of the class must be present for
+                            python classes.
+
+           grab_tweets  : This method is to utilize the existing connection
+                                 with the database and get the needed data
+
+           update_area : This method is used to call the game class and update
+                         the database entry for that specific area.
+	"""
+	def __init__(self):
+		"""
+		Class Constructor
+		"""
+		self.mongoclient = MongoClient('mongodb://localhost:27017') # Creates aa connection to MongoDB
+		self.CollState = client.search_results.States #Get State collection from search_results database
+		self.CollTweets = client.search_results.raw_data #Get raw_data collection from search_results database
+		self.TweetedStates = None
+
+	def grab_tweets(self):
+		Tweets = self.CollTweets.find({'place':{"$type":3}}) # Find the tweet that have geo coordinates for the moment.
+		for i in range(Tweets.count()):
+			tweet = Tweets[i] # Get tweet. This is a dictionary
+			place = tweet['place']# also a dictionary
+			name = place['full_name']
+			state = name[-2:]
+			#If the states is already found increment the number of tweets
+			#for this state else put 1 since this is the first tweet
+			if (self.TweetedStates.__contains__(state) and name['country_code']=='US'):
+				self.TweetedStates[state] = self.TweetedStates[state]+1
+			else:
+				self.TweetedStates[state] = 1
+
+
+	def update_area(self,tweets,area,name,motherstate=None):
+		if (area == 'state'):
+                        # Query the database for the state with id its name.
+			AreaEntry = self.CollState.find_one({'_id':name})
+		elif (area == 'county'):
+                        # Query the database for the state with id the motherstate
+                        # By taking the name field the proper collection for the
+			# the county leaderboard is selected.
+			state = self.CollState.find_one({'_id':,motherstate})
+			CollCounty = db[state['name']]
+			AreaEntry = collCounty.find_one('name':name)
+
+		#Grad the date of the last update, create an object of the Game
+		#class and call the method for the new points
+		lastdate = date(AreaEntry['lastUpdate'][0],AreaEntry['lastUpdate'][1],AreaEntry['lastUpdate'][2]) #extract last update date
+		GamingArea = Game(area,AreaEntry['name'],AreaEntry['points'],tweets,AreaEntry['lastTweettVal'],lastdate,AreaEntry['totalTweets'])
+		GamingArea.NewPoints()
+
+		#Change the values of the database entry based on the decisions
+		#made by the Game class object
+		AreaEntry['points']=GamingArea.__get__('points')
+		AreaEntry['lastTweetVal'] = GamingArea.__get__('lastTweetVal')
+		AreaEntry['lastUpdate'] = GamingArea.__get__('lastUpdate')
+		AreaEntry['totalTweets'] = GamingArea.__get__('totalTweets')
+
+		#Update the entry in the correct collection for the state or the county.
+		if area == 'state':
+			CollState.update({'_id':AreaEntry['_id']},AreaEntry,True)
+		elif area == 'county':
+			CollCounty.update({'_id':AreaEntry['_id']},AreaENtry,True)
+
 	
-	collTweets = db['#ski']
-	collStates = db.States #Selects a collection
-	tweets_with_places = collTweets.find({'place':{"$type":3}}) #query for tweets that have non null place. 
-	tweet = twets_with_places[0] # Get the first tweet. This is a dictionary
-	place = tweet['place']# also a dictionary
-	name = place['full_name']
-	state = name[-2:]
-	state = collStates.find_one({u'_id':u'New York'}) #Query MongoDB
-	StatePoints = state['points'] #extract points
-	StatelastUpdate = date(state['lastUpdate'][0],state['lastUpdate'][1],state['lastUpdate'][2]) #extract last update date
-	StateName = state['_id']
-	StateTotalTweets = state['totalTweets']
-	
-	coll.update({'_id':state['_id']},state,True) #mongodb update
+
--- 1_code/game/game.py
+++ 1_code/game/game.py
@@ -1,9 +1,38 @@
+#####################################################################
+# file : game.py
+# author: Ioannis Paraskevakos
+# version : 0.1
+# date: October 2014
+# description:
+#####################################################################
 from __future__ import division
 import pymongo
-from datetime import datetime
+from datetime import date
 
 class Game:
-	def __init__(self,area='State',name=None,points=0,tweets=None,lastTweetVal=None,lastIncrease=None,lastUpdate=None,totalTweets=None,motherState=None):
+	
+	"""
+		Game Class. This class is used by the play module to update
+		the points of an area
+
+		Attributes:
+		area : The type of the area. It can be either State or County
+		name : The name of the Area
+		points : The Number of points that the area has
+		tweets : The number of unused tweets for this specific area from the last update
+		lastTweetVal : The number of Tweets that were used for the last update
+		lastUpdate : This is the date of the last update with tweets.
+		totalTweets : The total number of tweets
+		motherState : In case of a County in which State it belongs
+
+		__init__() : Class Constractor
+		__get__() : returns the value of an attribute
+		__set__() : Sets an attribute
+		NewPoints : The basic point update method.
+	"""
+
+	def __init__(self,area='state',name=None,points=0,tweets=None,lastTweetVal=None,lastUpdate=None,totalTweets=None,motherState=None):
+		"""Class Constructor. The constructor initializes the classes attributes."""
 		self.area         = area
 		self.name         = name
 		self.points       = points
@@ -14,6 +43,9 @@ class Game:
 		self.motherState  = motherState
 
 	def __get__(self, attr='name'):
+		"""
+		Return the attribute based on the name of the attribute
+		"""
 		if attr == 'area':
 			return self.area
   		elif attr == 'name':
@@ -34,6 +66,9 @@ class Game:
 			return 0
 
 	def __set__(self, attr,val):
+		"""
+		Set the value of an attribute based on it name with val
+		"""
 		if attr == 'area':
 			self.area = val
 			return 1
@@ -64,41 +99,67 @@ class Game:
 
 	def NewPoints(self):
 		
-		TimeNow = datetime.now()
-		TimeDelta = TimeNow - self.lastUpdate
+		"""
+		The basic point update method. It updates the points of an area
+		based on how often it is updated, when tweets from that area are
+		retrieved, what impact those new tweets have in the total
+		number of tweets from that area and what is the difference from
+		the last update.
+		
+		"""
+
+		TimeNow = date.today() #Get today's date
+		TimeDelta = TimeNow - self.lastUpdate # Calculate the difference from last update
 		LastTweetCount = self.lastTweetVal
 		NewTweetCount = self.tweets
-		Update = 0
-		if (TimeDelta.days > 7 and self.tweets == 0 and self.totalTweets != 0):
+		Update = 0 # Indicates whether an update should happen or not.
+			   # Value 0 no update, 1 update everything, 2 update only points
+
+
+		if (TimeDelta.days > 7 and self.tweets == 0 and self.totalTweets != 0 and self.points > 0):
+			#If there is no new tweets for a week subtract 10 points and keep last update's
+			# date and last tweet count
 			EarnedPoints = -10
-			Update = 1
-			self.set('lastUpdate',TimeNow)
+			Update = 2
 		else:
+			# At the beginning consider the points to be as many as the new tweets minus the old
+			# This way the game system is just, less tweets from last time means loss of points
 			EarnedPoints = NewTweetCount - LastTweetCount
 			if(EarnedPoints > 0 and NewTweetCount/LastTweetCount >= 1.5 and NewTweetCount/self.totalTweets > 0.1):
+				# More than 50% extra tweets and mre than the 10% of all tweets 10 extra points
 				EarnedPoints = EarnedPoints + 10
 				Update = 1
-			elif (EarnedPoints > 0 and NewTweetCount/LastTweetCount >= 1.5 and NewTweetCount/self.totalTweets <= 0.1):
+			elif (EarnedPoints >= 0 and NewTweetCount/LastTweetCount >= 1.5 and NewTweetCount/self.totalTweets <= 0.1):
+				# More than 50% extra tweets and less than the 10% of all tweets 5 extra points
 				EarnedPoints = EarnedPoints + 5
 				Update = 1
-			elif (EarnedPoints > 0 and NewTweetCount/LastTweetCount < 1.5 and NewTweetCount/self.totalTweets > 0.1):
+			elif (EarnedPoints >= 0 and NewTweetCount/LastTweetCount < 1.5 and NewTweetCount/self.totalTweets > 0.1):
+				# Less than 50% extra tweets and mre than the 10% of all tweets 5 extra points
 				EarnedPoints = EarnedPoints + 5
 				Update = 1
-			elif (EarnedPoints > 0 and NewTweetCount/LastTweetCount < 1.5 and NewTweetCount/self.totalTweets <= 0.1):
+			elif (EarnedPoints >= 0 and NewTweetCount/LastTweetCount < 1.5 and NewTweetCount/self.totalTweets <= 0.1):
+				# Less than 50% extra tweets and less than the 10% of all tweets 0 extra points
 				Update = 1
 			elif (EarnedPoints < 0 and NewTweetCount/LastTweetCount >= 0.5 and NewTweetCount/self.totalTweets > 0.1):
+				# Less absolute value and more than 50% tweets and mre than the 10% of all tweets half points
 				EarnedPoints = 0.5*EarnedPoints
 				Update = 1
 			elif (EarnedPoints < 0 and NewTweetCount/LastTweetCount < 0.5 and NewTweetCount/self.totalTweets > 0.1):
+				# Less absolute value and less than 50% tweets and m0re than the 10% of all tweets three quarters points		
 				EarnedPoints = 0.75*EarnedPoints
 				Update = 1
 			else:
+				# Whatever is at the beginning
 				Update = 1
 
-		if (Update):
+		if (Update == 1):
+			#Update everything
 			self.points = self.points+EarnedPoints
 			self.lastTweetVal = NewTweetCount
 			self.totalTweets = self.totalTweets + NewTweetCount
 			self.lastUpdate = TimeNow
-
+		elif (Update == 2):
+			#Update points
+			self.points = self.points+EarnedPoints
 		
+
