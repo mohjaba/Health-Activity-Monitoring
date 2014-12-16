d0aa3325d0e4e553597270a524dbc9b5981c44b0
Jalal Abdulbaqi <jalal.nizar@gmail.com>
2014-12-16 11:14:19 -0500

    Update

---
+++ 1_code/interactive_map/Demo2/main.py
@@ -0,0 +1,100 @@
+"""A Flask server that presents a REST interface to the MongoDB geoNear command."""
+from flask import Flask, g, Response, send_from_directory, redirect, url_for
+import pymongo
+from pymongo.errors import ConnectionFailure
+from bson.son import SON
+import json
+from werkzeug.routing import FloatConverter as BaseFloatConverter
+from functools import wraps
+import os
+
+# Note: We don't need to call run() since our application is embedded within
+# the App Engine WSGI application server.
+app = Flask(__name__, static_folder='web')
+
+
+# Workaround a bug in the float conversion
+class FloatConverter(BaseFloatConverter):
+    regex = r'-?\d+(\.\d+)?'
+app.url_map.converters['float'] = FloatConverter
+
+# App config params (use local MongoDB install on default port)
+app.config.update(dict(
+    MONGO_DB_HOST='127.0.0.1',
+    MONGO_DB_PORT=27017,
+    MONGO_DB_NAME='test_db',
+    MONGO_DB_COLL='test_coll',
+    DIST_DIR=os.path.join(os.path.dirname(os.path.abspath(__file__)), 'dist')
+))
+
+
+# Annotation to set content type to text/json
+def returns_json(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        r = f(*args, **kwargs)
+        print repr(r)
+        (rsp, stat) = (r[0], r[1]) if isinstance(r, (tuple, list, set)) else (r, 200)
+        return Response(rsp, content_type='text/json', status=stat)
+    return decorated_function
+
+
+def mongodb():
+    """Opens a new database connection if there is none yet for the
+    current application context."""
+    if not hasattr(g, 'mongodb'):
+        g.mongodb = connect_mongo_db()
+    return g.mongodb
+
+
+def connect_mongo_db():
+    mongo_client = pymongo.MongoClient(app.config['MONGO_DB_HOST'], app.config['MONGO_DB_PORT'])
+    return mongo_client[app.config['MONGO_DB_NAME']]
+
+
+def coll_name():
+    return app.config['MONGO_DB_COLL']
+
+@app.route('/')
+def index():
+    return redirect("web/index.html")
+
+@app.route('/all/points/near/<float:lat>/<float:lon>')
+@returns_json
+def near(lat, lon):
+    points = mongodb().command(SON([('geoNear', coll_name()), ('near', [lon, lat]), ('spherical', True)]))['results']
+    rsp = {"points":  list(serialize_mongo_results(points))}
+    return json.dumps(rsp)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    """Return a custom 404 error."""
+    return 'Sorry, Nothing at this URL.', 404
+
+
+@app.errorhandler(ValueError)
+@returns_json
+def handle_value_error(error):
+    return json.dumps({"message": repr(error)}), 400
+
+
+@app.errorhandler(ConnectionFailure)
+@returns_json
+def handle_connection_failure(error):
+    return json.dumps({"message": repr(error)}), 500
+
+
+# Custom static data--minified script
+@app.route('/dist/g11-maps.min.js')
+def g11_maps_js():
+    return send_from_directory(app.config['DIST_DIR'], 'g11-maps.min.js')
+
+
+# Utility methods
+def serialize_mongo_results(results):
+    for r in results:
+        o = r['obj']
+        o['dis'] = r['dis']
+        o['_id'] = str(o['_id'])
+        yield o
---
+++ 1_code/interactive_map/Demo2/web/js/g11-maps.js
@@ -0,0 +1,195 @@
+/**
+ * A JS file for creating declarative Google Maps bound to a REST data source.
+ * 
+ * Depends on the GoogleMaps API, and includes underscore and promisejs, no jQuery required. 20KB minimized. 
+ */
+var _ = require('../bower_components/underscore/underscore.js');
+var promise = require('../bower_components/promisejs/promise.js').promise;
+
+// Base URL for the REST service
+var BASE_POINTS_URL = "/all/points/near/";
+
+// Default map location: Sunset Beach Bar
+var DEFAULT_LAT = 39.828127;
+var DEFAULT_LNG = -98.579404;
+
+// CSS class for where to insert Google Map
+var MAP_CLASS_NAME = "g11-google-map";
+
+// Initialize page on load
+google.maps.event.addDomListener(window, 'load', initialize_page_map_elements);
+function initialize_page_map_elements() {
+	// Find all declared map elements and assign a new G11 Map to each
+	_.each(document.getElementsByClassName(MAP_CLASS_NAME), function(el) {
+		// Convert data-options attribute to map options
+		LOG.debug("Initializing Google Map element");
+		//LOG.debug(el);
+		var g11map = new G11Map(el, { zoom: 12 });
+		// Add a listener so that if the center of the map is moved, we fetch markers.
+		google.maps.event.addListener(g11map.map, 'center_changed', function() {
+			// Ignore the event if a timeout is pending
+			if (g11map.timeout !== null) {
+				return;
+			}
+			// Set a timeout to update the map
+			g11map.timeout = window.setTimeout(function() {
+				c = g11map.map.getCenter();
+				g11map.loadMarkersFor(c.lat(), c.lng());
+				g11map.timeout = null;
+			}, 500);
+		});
+	});
+}
+
+
+/**
+ * A wrapper for Google maps that supports loading/rendering markers.
+ */
+function G11Map(el, mapOptions) {
+	this.mapOptions = mapOptions;
+	this.map = new google.maps.Map(el, mapOptions);
+	this.points = [];
+	this.markers = {};
+	this.timeout = null;
+	// Explicit lat/lon provided
+	if (mapOptions.lat && mapOptions.lon) {
+		this.setCenter(mapOptions.lat, mapOptions.lon);
+	// Try HTML5 geolocation
+	} else if (navigator.geolocation) {
+		var self = this;
+		navigator.geolocation.getCurrentPosition(function(position) {
+			self.setCenter(position.coords.latitude, position.coords.longitude);
+		}, function() {
+			self.setCenter(DEFAULT_LAT, DEFAULT_LNG);
+		});
+	// Browser doesn't support Geolocation
+	} else {
+		this.setCenter(DEFAULT_LAT, DEFAULT_LNG);
+	}
+}
+
+/**
+ * Centers the map on the given lat/lng
+ */
+G11Map.prototype.setCenter = function(lat, lng) {
+	var nc = new google.maps.LatLng(lat, lng);
+	var cc = this.map.getCenter();
+	if (nc && cc && nc.lat() == cc.lat() && nc.lng() == cc.lng()) {
+		return; // No update
+	}
+	this.map.setCenter(nc);
+};
+
+
+/**
+ * Loads markers for the given lat/lng 
+ */
+G11Map.prototype.loadMarkersFor = function (lat, lng) {
+	LOG.debug('Loading markers for ' + lat + ',' + lng);
+	var self = this;
+	var url = BASE_POINTS_URL + lat + "/" + lng;
+	promise.get(url).then(function(error, data, xhr) {
+		if (error) {
+			LOG.warn('Geolocation query '+url+' returned status ' + xhr.status+': ');
+			if (data) {
+				LOG.warn(data);
+			}
+			return;
+		}
+		self.renderMarkerData(JSON.parse(data).points);
+	});
+};
+
+/**
+ * Renders the given marker data on the map, removing existing markers 
+ */
+G11Map.prototype.renderMarkerData = function(points) {
+	LOG.debug('Received ' + points.length + ' points' );
+	//LOG.debug(points);
+	points = _.sortBy(points, function(p) {return p._id;}); // Sort the incoming points by ID
+	var ar = added_and_removed(this.points, points, function(o, n) { return o._id.localeCompare(n._id); }); // Compare incoming with current
+	var added = ar[0];
+	var removed = ar[1];
+	var self = this;
+	// For each removed point, remove the corresponding marker
+	_.each(removed, function(p) {
+		self.markers[p._id].setMap(null);
+		self.markers[p._id] = null;
+	});
+	// For each added point, add a marker
+	_.each(added, function(p) {
+		// Create marker
+		var coord = p.coordinates.coordinates;
+		var marker = new google.maps.Marker({
+			position : new google.maps.LatLng(coord[1], coord[0]),
+			map : self.map,
+			title : p.title,
+			icon: 'images/blue_dot.png'
+		});
+		self.markers[p._id] = marker;
+		// Create an info box
+		var infowindow = new google.maps.InfoWindow({
+			content : '<a href="' + p.url + '"><h3>' + p.title + '</h3></a><p>Source: '+p.source+'</p>'
+		});
+		// Show info box on point click
+		google.maps.event.addListener(marker, 'click', function() {
+			infowindow.open(self.map, marker);
+		});
+	});
+	
+	this.points = points;
+};
+
+// Utility functions
+
+/**
+ * Simple, browser-safe logging 
+ */
+var LOG = (function() {
+	var log = function(s) {
+		if (typeof(console) != "undefined") { console.log(s); }
+	};
+	return {
+		debug_on: true,
+		debug: function(s) {
+			if (this.debug_on) { log(s); }
+		},
+		warn: function(s) { log(s); }
+	};
+})();
+
+/**
+ * Accepts 2 lists and the comparison func by which they have been sorted,
+ * and returns 2 lists:
+ * 
+ *  - The list of items that have been added
+ *  - The list of items that have been removed
+ */
+function added_and_removed(old_list, new_list, cmp) {
+	var added = [], removed = [];
+	var ptro = 0, ptrn = 0;
+	while (ptro < old_list.length && ptrn < new_list.length) {
+		// Item is missing from new
+		if (cmp(old_list[ptro], new_list[ptrn]) < 0) { 
+			removed.push(old_list[ptro]);
+			ptro += 1;
+		// Item is newly added
+		} else if (cmp(old_list[ptro], new_list[ptrn]) > 0) { 
+			added.push(new_list[ptrn]);
+			ptrn += 1;
+		// Items match
+		} else {
+			ptro += 1;
+			ptrn += 1;
+		}
+	}
+	// If old list has tail, they were all removed
+	if (ptro < old_list.length) {
+		removed = removed.concat(old_list.slice(ptro));
+	}
+	// If new list has tail, they were all added
+	if (ptrn < new_list.length) {
+		added = added.concat(new_list.slice(ptrn));
+	}
+	return [ added, removed ];
+}
