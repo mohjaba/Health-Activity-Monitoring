4ec2fb0f31d5d8f880d2b27f75191729799f3096
KeDong92 <Ke.Dong92@gmail.com>
2014-12-16 11:56:37 -0500

    Update temp.py

--- 1_code/www/Recommendation_files/temp.py
+++ 1_code/www/Recommendation_files/temp.py
@@ -3,35 +3,34 @@ import sys
 
 owm = pyowm.OWM('80c233e546bdab004385a95fd8f98eee')
 
-#def get_newname(name):
-#	return name.replace(":", " ")
+def get_newname(name):
+	return name.replace(":", " ")
 
 
 
 if __name__ == '__main__':
 
 
-#	name = sys.argv[1]
-#	newname = get_newname(name)
-#	print newname
+	name = sys.argv[1]
+	newname = get_newname(name)
+	print newname
 
 	
-#	observation = owm.weather_at_place(newname)
-#	w = observation.get_weather()
+	observation = owm.weather_at_place(newname)
+	w = observation.get_weather()
 	
-#	winfo = []
-#	winfo.append(newname)
-#	winfo.append(w.get_temperature(unit='fahrenheit')['temp'])
-#	winfo.append(w.get_status())
-#	winfo.append(w.get_wind()['speed'])
-#	winfo.append(w.get_humidity())
-#	winfo.append(w.get_pressure()['press'])
+	winfo = []
+	winfo.append(newname)
+	winfo.append(w.get_temperature(unit='fahrenheit')['temp'])
+	winfo.append(w.get_status())
+	winfo.append(w.get_wind()['speed'])
+	winfo.append(w.get_humidity())
+	winfo.append(w.get_pressure()['press'])
 	
-#	print(winfo[1])
-#	print(winfo[2])
-#	print(winfo[3])
-#	print(winfo[4])
-#	print(winfo[5])
-call = "what is going on"
+	print(winfo[1])
+	print(winfo[2])
+	print(winfo[3])
+	print(winfo[4])
+	print(winfo[5])
 
-print call
+
